4ebea10e501a90f45550414e5f52656092d5d02b
daudi13 <davouma13@gmail.com>
2022-02-16 13:04:49 +0300

    Add .js file

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as selectors from './modules/selectors.js';
+import _ from 'lodash';
+import './style.css';
+
+class updateUi {
+	static block = [];
+
+	static enterBtn = selectors.enterBtn;
+
+	static input = selectors.input;
+
+	static listblock = selectors.listblock;
+
+	static taskArr = selectors.taskArr;
+
+	// populating the ul with task 
+
+	static addTasks(arrs) {
+		updateUi.listblock.innerHTML = ' ';
+		arrs.forEach((arr) => {
+			const htmlTemp = `
+			<li class="list-item" id="${arr.index}"><input type="checkbox" name="todo-1"><p contenteditable="true">${arr.desc}</p><button><i class="fas fa-ellipsis-v"></i></button></li>
+			`;
+			updateUi.listblock.insertAdjacentHTML('afterbegin', htmlTemp)
+		});
+	}
+}
+
+window.onload = updateUi.addTasks(selectors.taskArr);
