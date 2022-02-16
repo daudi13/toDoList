3aca55ad89585a3c5ba246c533933913a32a91c1
daudi13 <davouma13@gmail.com>
2022-02-16 18:48:31 +0300

    Add addTask functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import _ from 'lodash';
+import _, { update } from 'lodash';
+import NewItem from './modules/listClass.js';
 import * as selectors from './modules/selectors.js';
 import './style.css';
 
 class updateUi {
-	static block = [];
+
+	static taskArr = []
 
 	static enterBtn = selectors.enterBtn;
 
@@ -11,19 +13,31 @@ class updateUi {
 
 	static listblock = selectors.listblock;
 
-	static taskArr = selectors.taskArr;
-
 	// populating the ul with task
+
+	static updateTask() {
+		const taskItem = new NewItem(updateUi.input.value);
+		updateUi.taskArr.push(taskItem);
+		updateUi.input.value = '';
+		updateUi.addTasks(updateUi.taskArr);
+	}
 
 	static addTasks(arrs) {
 	  updateUi.listblock.innerHTML = ' ';
 	  arrs.forEach((arr) => {
 	    const htmlTemp = `
-			<li class="list-item" id="${arr.index}"><input type="checkbox" name="todo-1"><p contenteditable="true">${arr.desc}</p><button><i class="fas fa-ellipsis-v"></i></button></li>
+			<li class="list-item" id="${arr.index}"><input type="checkbox" name="todo-1"><p contenteditable="true">${arr.task}</p><button><i class="fas fa-ellipsis-v"></i></button></li>
 			`;
 	    updateUi.listblock.insertAdjacentHTML('afterbegin', htmlTemp);
 	  });
 	}
 }
 
-window.onload = updateUi.addTasks(selectors.taskArr);
+updateUi.enterBtn.addEventListener('click', (e) => {
+	e.preventDefault()
+	if (updateUi.input.value === '') {
+		alert(`Please add a task`);
+	} else {
+		updateUi.updateTask()
+	}
+})
---
+++ src/modules/listClass.js
@@ -0,0 +1,9 @@
+
+export default class NewItem {
+	constructor(task) {
+		this.task = task,
+			this.complete = false,
+			this.index = 0;
+	}
+}
+
