27a7b2581ee223ef613deddd87b0f2b3665f8e79
daudi13 <davouma13@gmail.com>
2022-02-18 16:36:34 +0300

    Update linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import _, {} from 'lodash';
 import { updateSave } from './modules/interactive.js';
 import UpdateUi from './modules/mainSec.js';
--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -1,20 +1,20 @@
-import UpdateUi from "../modules/mainSec.js";
+import UpdateUi from './mainSec.js';
 
 // status update
 
-export function clear () {
-	const newArr = UpdateUi.taskArr.filter(tasker => tasker.complete !== true);
-	localStorage.setItem('taskItems', JSON.stringify(newArr));
-	UpdateUi.addTasks(newArr);
-	location.reload();
+export function clear() {
+  const newArr = UpdateUi.taskArr.filter((tasker) => tasker.complete !== true);
+  localStorage.setItem('taskItems', JSON.stringify(newArr));
+  UpdateUi.addTasks(newArr);
+  location.reload();
 }
 
 export function updateSave() {
-		if (localStorage.getItem('taskItems')) {
-			UpdateUi.taskArr = JSON.parse(localStorage.getItem('taskItems'));
-			UpdateUi.addTasks(UpdateUi.taskArr);
-		} else {
-			localStorage.setItem('taskItems', '');
-			UpdateUi.taskArr = [];
-		}
-	}
+  if (localStorage.getItem('taskItems')) {
+    UpdateUi.taskArr = JSON.parse(localStorage.getItem('taskItems'));
+    UpdateUi.addTasks(UpdateUi.taskArr);
+  } else {
+    localStorage.setItem('taskItems', '');
+    UpdateUi.taskArr = [];
+  }
+}
