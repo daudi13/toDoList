dbe219dd4fc310cf5daa0d08e2fb422dd2d1573c
daudi13 <davouma13@gmail.com>
2022-02-21 11:27:01 +0300

    Add github link

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
 		clean: true,
-		publicPath: "/toDoList/",
+		publicPath: "/https://github.com/daudi13/toDoList/",
 
   },
   module: {
